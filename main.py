f2f18ec44094d9b5d9971df56c05be4d4e1fa413
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-07 20:45:22 +0300

    4-й коммит

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from currency_converter import CurrencyConverter
+from telebot import types
 
 from settings import TELEGRAM_TOKEN
 
@@ -7,12 +8,41 @@ conversion = CurrencyConverter
 
 converter_bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+amount = 0
+
 
 @converter_bot.message_handler(commands=["start"])
 def start(message):
     """ Функция приветствия пользователя """
-    hello = f"Привет, {message.from_user.first_name}рад тебя видеть!👋"
+    hello = (f"Привет, {message.from_user.first_name}, рад тебя видеть!👋\n"
+             f"Введите сумму для конвертации")
     converter_bot.send_message(message.chat.id, hello)
+    converter_bot.register_next_step_handler(message, conversion_amount)
+
+
+def conversion_amount(message):
+    """ Функция для конвертации валют """
+    global amount
+    try:
+        amount = int(message.text.strip())
+    except ValueError:
+        info_message_2 = "Некорректный ввод, введите сумму!"
+        converter_bot.send_message(message.chat.id, info_message_2)
+        converter_bot.register_next_step_handler(message, conversion_amount)
+        return
+
+    if amount > 0:
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        btn1 = types.InlineKeyboardButton("RUB/USD", callback_data="rub/usd")
+        btn2 = types.InlineKeyboardButton("RUB/EUR", callback_data="rub/eur")
+        btn3 = types.InlineKeyboardButton("Другая валютная пара", callback_data="else")
+        markup.add(btn1, btn2, btn3)
+        info_message = "Выберите валютную пару или введите свою"
+        converter_bot.send_message(message.chat.id, info_message, reply_markup=markup)
+    else:
+        info_message_3 = "Некорректный ввод, сумма должна быть больше ноля!"
+        converter_bot.send_message(message.chat.id, info_message_3)
+        converter_bot.register_next_step_handler(message, conversion_amount)
 
 
 converter_bot.polling(none_stop=True)
