307b6c4a33ddb54ed9b362e25095d98a4a38872f
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-07 20:19:49 +0300

    3-й коммит, библиотека CurrencyConverter

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import telebot
+from currency_converter import CurrencyConverter
 
 from settings import TELEGRAM_TOKEN
 
+conversion = CurrencyConverter
+
 converter_bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
 
 @converter_bot.message_handler(commands=["start"])
 def start(message):
     """ Функция приветствия пользователя """
     hello = f"Привет, {message.from_user.first_name}рад тебя видеть!👋"
     converter_bot.send_message(message.chat.id, hello)
+
+
+converter_bot.polling(none_stop=True)
